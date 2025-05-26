76b65abd15b25ae5e1573b91a669e3195bb64ac6
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-05-26 23:06:19 +0200

    feat: Create Card model

---
+++ src/main/java/dev/kandv/kango/models/Card.java
@@ -0,0 +1,70 @@
+package dev.kandv.kango.models;
+
+import dev.kandv.kango.models.enums.Color;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Card {
+    private String title;
+    private String description;
+    private Color color;
+    private List<AttachedFile> attachedAttachedFiles = new ArrayList<>();
+    private Date deadLine;
+    private List<Check> checks = new ArrayList<>();
+    // TODO Add Tag List
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class AttachedFile {
+        private String fileName;
+        private String fileUrl;
+    }
+
+    @Getter
+    @Setter
+    public static class Check {
+        private String label;
+        private boolean checked;
+        private int position;
+
+        public Check(String label, boolean checked) {
+            this.label = label;
+            this.checked = checked;
+        }
+    }
+
+    public void attachFile(AttachedFile attachedFile){
+        this.attachedAttachedFiles.add(attachedFile);
+    }
+
+    public void detachFile(AttachedFile attachedFile){
+        this.attachedAttachedFiles.remove(attachedFile);
+    }
+
+    public void addCheckToCheckList(Check check){
+        check.setPosition(this.checks.size());
+        this.checks.add(check);
+    }
+
+    public void removeCheckFromCheckList(Check check) {
+        this.checks.remove(check);
+        for (int i = 0; i < this.checks.size(); i++) {
+            this.checks.get(i).setPosition(i);
+        }
+    }
+
+    public void updateCheckFromCheckList(Check check){
+        this.checks.set(check.position, check);
+    }
+}
---
+++ src/test/java/dev/kandv/kango/units/models/CardTest.java
@@ -0,0 +1,122 @@
+package dev.kandv.kango.units.models;
+
+import dev.kandv.kango.models.Card;
+import dev.kandv.kango.models.enums.Color;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CardTest {
+
+    @Test
+    public void testSetTitle() {
+        String expectedTitle = "Example Title";
+        Card card = new Card();
+
+        card.setTitle(expectedTitle);
+
+        String resultTitle = card.getTitle();
+        assertThat(resultTitle).isEqualTo(expectedTitle);
+    }
+
+    @Test
+    public void testSetDescription() {
+        String expectedDescription = "Example Description";
+        Card card = new Card();
+
+        card.setDescription(expectedDescription);
+
+        String resultDescription = card.getDescription();
+        assertThat(resultDescription).isEqualTo(expectedDescription);
+    }
+
+    @Test
+    public void testSetColor() {
+        Color expectedColor = Color.RED;
+        Card card = new Card();
+
+        card.setColor(expectedColor);
+
+        Color resultColor = card.getColor();
+        assertThat(resultColor).isEqualTo(expectedColor);
+    }
+
+    @Test
+    public void testAttachFile() {
+        Card.AttachedFile expectedAttachedFile = new Card.AttachedFile("example.png", "example");
+        Card card = new Card();
+
+        card.attachFile(expectedAttachedFile);
+        List<Card.AttachedFile> attachedFiles = card.getAttachedAttachedFiles();
+
+        assertThat(attachedFiles).hasSize(1);
+        Card.AttachedFile attachedFile = attachedFiles.getFirst();
+        assertThat(attachedFile).isEqualTo(expectedAttachedFile);
+    }
+
+    @Test
+    public void testDetachFile() {
+        Card.AttachedFile expectedAttachedFile = new Card.AttachedFile("example.png", "example");
+        Card card = new Card();
+
+        card.attachFile(expectedAttachedFile);
+        List<Card.AttachedFile> attachedFiles = card.getAttachedAttachedFiles();
+
+        assertThat(attachedFiles).hasSize(1);
+        card.detachFile(expectedAttachedFile);
+
+        assertThat(attachedFiles).isEmpty();
+    }
+
+    @Test
+    public void testEndDate() {
+        Date expectedDate = new Date();
+        Card card = new Card();
+
+        card.setDeadLine(expectedDate);
+        Date resultDate = card.getDeadLine();
+
+        assertThat(resultDate).isEqualTo(expectedDate);
+    }
+
+    @Test
+    public void testAddCheckToCheckList() {
+        Card card = new Card();
+        Card.Check check = new Card.Check("Example", false);
+
+        card.addCheckToCheckList(check);
+        List<Card.Check> checks = card.getChecks();
+        assertThat(checks).hasSize(1);
+    }
+
+    @Test
+    public void testRemoveCheckFromCheckList() {
+        Card card = new Card();
+        Card.Check check = new Card.Check("Example", false);
+        card.addCheckToCheckList(check);
+        List<Card.Check> checks = card.getChecks();
+        assertThat(checks).hasSize(1);
+
+        card.removeCheckFromCheckList(check);
+        checks = card.getChecks();
+        assertThat(checks).isEmpty();
+    }
+
+    @Test
+    public void testUpdateCheckFromCheckList() {
+        Card card = new Card();
+        Card.Check check = new Card.Check("Example", false);
+        card.addCheckToCheckList(check);
+
+        check.setChecked(true);
+        card.updateCheckFromCheckList(check);
+
+        List<Card.Check> checks = card.getChecks();
+        assertThat(checks).hasSize(1);
+        assertThat(checks.getFirst()).isEqualTo(check);
+    }
+}
