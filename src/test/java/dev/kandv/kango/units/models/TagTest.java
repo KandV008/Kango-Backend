b80afa585ab184fb311b48ec9c23b8562114cce5
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-05-31 13:05:11 +0200

    feat: Create Tag Model

---
+++ src/test/java/dev/kandv/kango/units/models/TagTest.java
@@ -0,0 +1,34 @@
+package dev.kandv.kango.units.models;
+
+import dev.kandv.kango.models.Tag;
+import dev.kandv.kango.models.enums.Color;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class TagTest {
+    private Tag tag;
+
+    @BeforeEach
+    public void beforeEach(){
+        this.tag = new Tag("Example", Color.PURPLE);
+    }
+
+    @Test
+    public void testSetAndGetLabel(){
+        String expected = "Example Label";
+        this.tag.setLabel(expected);
+        String actual = this.tag.getLabel();
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void testSetAndGetColor(){
+        Color expected = Color.GREEN;
+        this.tag.setColor(expected);
+        Color actual = this.tag.getColor();
+        assertThat(actual).isEqualTo(expected);
+    }
+}
