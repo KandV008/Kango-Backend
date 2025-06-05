11afec75e42fa1b74049a4f2abe1d36c2dd38cca
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-06-05 17:24:47 +0200

    feat: Update AttachedFIle embeddable

--- src/main/java/dev/kandv/kango/models/utils/AttachedFile.java
+++ src/main/java/dev/kandv/kango/models/utils/AttachedFile.java
@@ -1,13 +1,27 @@
 package dev.kandv.kango.models.utils;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Embeddable;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class AttachedFile {
+    @NonNull
     private String fileName;
+    @NonNull
     private String fileUrl;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof AttachedFile )) {
+            return false;
+        }
+
+        boolean checkFileName = this.fileName.equals(((AttachedFile) obj).fileName);
+        boolean checkFileUrl = this.fileUrl.equals(((AttachedFile) obj).fileUrl);
+        return checkFileName && checkFileUrl;
+    }
 }
