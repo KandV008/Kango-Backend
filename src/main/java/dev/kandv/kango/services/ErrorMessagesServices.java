f619a1228a7405e531f0f3d26e0407d2d6b463c7
Krokodile <117300490+KandV008@users.noreply.github.com>
2025-06-24 18:22:30 +0200

    feat: Create State Service

--- src/main/java/dev/kandv/kango/services/ErrorMessagesServices.java
+++ src/main/java/dev/kandv/kango/services/ErrorMessagesServices.java
@@ -8,5 +8,5 @@ public class ErrorMessagesServices {
     public static final String NOT_FOUND_DASHBOARD_WITH_ID_ERROR = "ERROR: There is no Dashboard with such ID. ID: ";
     public static final String NOT_FOUND_TABLE_WITH_ID_ERROR = "ERROR: There is no Table with such ID. ID: ";
     public static final String NOT_FOUND_TAG_WITH_ID_ERROR = "ERROR: There is no Tag with such ID. ID: ";
-
+    public static final String NOT_FOUND_STATE_ERROR = "ERROR: There is no State";
 }
